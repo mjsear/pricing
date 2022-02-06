fa478edbd55eb50fbff0726244425068075a79f6
mjsear <88648273+mjsear@users.noreply.github.com>
2022-02-06 22:00:51 +0000

    Updating repo with basic file in progress

--- pricing.py
+++ pricing.py
@@ -1,3 +1,6 @@
+from cmath import nan
+
+
 def main():
     import numpy as np
     import pandas as pd
@@ -5,43 +8,49 @@ def main():
     with open("D:\\Downloads\\am92rates.csv") as f:
         rates = pd.read_csv(f, header=0, names = ["sel","sel-1","ult"], index_col=0)
 
-    rates.rename(columns = {"q[x]":"sel", "q[x-1]+1":"sel-1"},inplace = True)
+    rates.rename(columns = {"q[x]":"sel", "q[x-1]+1":"sel-1"}, inplace = True)
     rates.columns
 
-    def annuity_due(x):
-        v = 1.04
-        if rates.Age.iloc[0] <= x <= rates.Age.iloc[-1]:
-            
-            start = 1+x-rates.Age[0]
-            epv = 1
-            for i in range(103-start):
-                epv += v**(-i) * (1-rates.qx.iloc[start+i])
-            print(start)
-            print(rates.Age.iloc[start])
-            return epv
+    def annuity_due(x, sel=len(rates.columns)-1):
+        v = 1.04**-1
+        axd = 0
+        if sel==len(rates.columns)-1:
+            for i in range(len(rates.columns)-1):
+                coef = 1
+                for j in range(i):
+                    coef *= (1-rates.iloc[j,j])
+                axd += coef * v**(x+i) * l(x)
+            axd += sum([1.04**(-1*i)*l(i,0) for i in np.arange(x+(len(rates.columns)-1),121,1)])
+            axd /= v**x * l(x)
+            #v**(x+0) * l(x) + 
+            #v**(x+1) * l(x) * (1-rates.iloc[0,0]) + 
+            #v**(x+2) * l(x) * (1-rates.iloc[0,0]) * (1-rates.iloc[1,1])
         else:
-            print("Age out of range")
-            return
-
+            axd = sum([1.04**(-1*i)*l(i, 0) for i in np.arange(x,121,1)])/(v**x * l(x,0))
+        return axd
+    annuity_due(80)
+    
     # product
-    def l(x, sel):
+    def l(x, sel=len(rates.columns)-1):
+        """
+        This should probably be refactored such that sel=0 means select and sel=len(rates.columns-1) means ultimate, all these definitions are currently awkward as hell
+        sel = 0 for ultimate mortality, sel = len(rates.columns)-1 for select mortality, sel = between those for years since selection
+        """
         if x in rates.index:
-            if sel == "ult":
-                return (1-rates.iloc[0:(x-rates.index[0]),-1:]).prod(axis=0)
-            elif sel == "sel":
-                lx = (1-rates["ult"].loc[x+len(rates.columns)-1]).prod(axis=0) #We go from 0 to (x-age[0]) plus extra steps for each select year, minus 1 for the ult year
-                for i in range(1,len(rates.columns)):
-                    lx /= (1-rates.iloc[x-rates.index[0]+len(rates.columns)-1-i,-1-i]) #Then we divide out the select q's to get back up from x+select to [x]
+            if pd.isna(rates.iloc[:,(len(rates.columns)-1) - sel].loc[x]):
+                raise IndexError("Age out of select range")
+            else:
+                if x == rates.index[0] and sel == 0:
+                    return 1
+                lx = l(x-1+sel,0)*(1-rates['ult'].loc[x-1+sel]) # (x-1) here is the basic unit, we are using the previous q rate to go back one
+                for i in range(1, sel+1):
+                    lx /= (1-rates.iloc[:,len(rates.columns)-1-i].loc[x+sel-i]) # here we are already starting at the destination, so no -1 is needed
                 return lx
         else:
             raise IndexError("Age out of table range")
 
     print(annuity_due(55))
 
-np.
-
-
 
 if __name__ == '__main__':
     main()
-
