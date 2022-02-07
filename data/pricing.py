79a02f72ed4a777b302df289c68247199b1aa14f
mjsear <michael.sear@gmail.com>
2022-02-07 13:56:54 +0000

    More neatening and adding niceties like docstrings

--- data/pricing.py
+++ data/pricing.py
@@ -1,17 +1,24 @@
-#from cmath import nan
-
-
 def main():
     import numpy as np
     import pandas as pd
 
-    with open("am92rates.csv") as f:
-        rates = pd.read_csv(f, header=0, names = ["sel","sel-1","ult"], index_col=0)
+    with open("am92rates.csv") as f: #THIS STUFF HAS TO BE CHANGED TO ALLOW FOR MULTIPLE RATES FILES
+        rates = pd.read_csv(f, header=0, names = ["sel","sel-1","ult"], index_col=0) #THE COLUMN NAMES SHOULD BE UNUSED, EXCEPT IN IN-PROGRESS COMMANDS
 
     rates.rename(columns = {"q[x]":"sel", "q[x-1]+1":"sel-1"}, inplace = True)
     rates.columns
 
     def annuity_due(x, sel=len(rates.columns)-1):
+        """
+        Returns the expected present value of an lifetime annuity paying amount 1 annually for a life aged x
+
+        Keyword arguments
+        x   -- Age of the life
+        sel -- Remaining years of selection 
+
+        Sel = 0 means ultimate, increasing by one for each year of selection REMAINING.
+        Thus, the default sel={number of columns - 1} is a just-selected life [x].
+        """
         v = 1.04**-1
         axd = 0
         if sel==len(rates.columns)-1:
@@ -22,19 +29,22 @@ def main():
                 axd += coef * v**(x+i) * l(x)
             axd += sum([1.04**(-1*i)*l(i,0) for i in np.arange(x+(len(rates.columns)-1),121,1)])
             axd /= v**x * l(x)
-            #v**(x+0) * l(x) + 
-            #v**(x+1) * l(x) * (1-rates.iloc[0,0]) + 
-            #v**(x+2) * l(x) * (1-rates.iloc[0,0]) * (1-rates.iloc[1,1])
         else:
             axd = sum([1.04**(-1*i)*l(i, 0) for i in np.arange(x,121,1)])/(v**x * l(x,0))
         return axd
     
-    
-    # product
     def l(x, sel=len(rates.columns)-1):
         """
-        This should probably be refactored such that sel=0 means select and sel=len(rates.columns-1) means ultimate, all these definitions are currently awkward as hell
-        sel = 0 for ultimate mortality, sel = len(rates.columns)-1 for select mortality, sel = between those for years since selection
+        Return the *percentage* of lives still living at age x. 
+        
+        Keyword arguments:
+        x   -- Age of the life
+        sel -- Remaining years of selection 
+
+        Sel = 0 means ultimate, increasing by one for each year of selection REMAINING.
+        Thus, the default sel={number of columns - 1} is a just-selected life [x].
+
+        This should probably be refactored such that sel=0 means select and sel=len(rates.columns-1) means ultimate, the current definitions are awkward as hell
         """
         if x in rates.index:
             if pd.isna(rates.iloc[:,(len(rates.columns)-1) - sel].loc[x]):
